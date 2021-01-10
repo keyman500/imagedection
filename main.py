18f7d24c77c90b0fc7f48632c37c30395a237147
Marc Hypolite <mar-chy@hotmail.com>
2021-01-10 02:31:14 -0400

    saves image

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ for classId,confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
     cv2.rectangle(img,box,color=(0,255,0),thickness=2)
     cv2.putText(img,classNames[classId-1],(box[0]+10,box[1]+30),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
     print(classNames[classId-1])
-    
+
+cv2.imwrite("mrc-analised.png",img)
 cv2.imshow("Output",img)
 cv2.waitKey(0)
